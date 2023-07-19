e8d2839527ed79ebee7146ec24aca8ef411115f3
saidonnet <saidonnet@gmail.com>
2023-07-19 16:59:48 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,45 +1,29 @@
-# OpenAI Chat Enhancer
+# OpenAI Chat Width Expander
 
-This is a Chrome extension designed to enhance the user experience on chat.openai.com. 
-
-## Features
-
-- **Full-width Text**: This extension adjusts the website's CSS to display text as wide as the screen.
-
-- **Quick Copy**: Adds a floating button labelled 'C' that, when clicked, copies the entire last generated message to your clipboard.
-
-- **Text-to-Speech**: Adds a floating button labelled 'S' that, when clicked, reads aloud the last generated message in the selected language. This is particularly useful when multitasking or wanting to rest your eyes. 
-
-- **Speech Stop**: Adds a floating button labelled 'T' that, when clicked, stops the current text-to-speech process.
-
-- **Language Selector**: Adds a dropdown selector to choose among English, French, and Arabic languages for the Text-to-Speech feature.
+OpenAI Chat Width Expander is a Chrome extension that removes width restrictions on chat.openai.com, enhancing readability and user experience.
 
 ## Installation
 
-This extension is not available on the Chrome Web Store, but you can install it manually by following these steps:
+Since this extension is not published on the Chrome Web Store, you can install it manually by following these steps:
 
-1. Download or clone this repository to your local machine.
-2. Open Google Chrome and go to `chrome://extensions`.
-3. Toggle on the "Developer mode" switch in the top right corner.
-4. Click "Load unpacked" and select the directory of this extension.
-5. Make sure the extension is enabled.
+1. Download the code from this GitHub repository by clicking the "Code" button at the top of the page, then "Download ZIP". Unzip the downloaded file.
 
-You should now see the extension's icon in the Chrome toolbar. You can pin it for quicker access.
+2. Open Google Chrome and go to the Extensions page (`chrome://extensions`).
+
+3. At the top right, turn on Developer mode.
+
+4. Click "Load unpacked".
+
+5. Find and select the unzipped folder that contains the extension's files.
+
+6. The extension should now be loaded and ready for use. You should see the extension's icon at the top right of the Chrome browser window.
 
 ## Usage
 
-Once you install the extension and navigate to chat.openai.com, you will see the floating buttons on your screen:
+Once the extension is installed, it will automatically apply the necessary CSS changes when you visit chat.openai.com. The chat's width restrictions will be removed, allowing the chat to utilize the full width of your screen.
 
-- The 'C' button copies the last generated message to your clipboard.
-- The 'S' button starts reading the last generated message aloud in the selected language.
-- The 'T' button stops the reading.
-- The dropdown above 'S' button allows you to choose the language for text-to-speech. 
+## License
 
-## Tooltips
+This project is open source without any licence restriction.
 
-Each floating button and the dropdown selector have tooltips that appear when you hover over them:
-
-- 'C' button: "Copy last generated message"
-- 'S' button: "Read last generated message"
-- 'T' button: "Stop reading"
-- Dropdown: "Select language for text-to-speech"
+Use and edit it as you need !
--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 const style = document.createElement('style');
 style.textContent = `
 ::-webkit-scrollbar {
-	width: 1rem;
+    width: 1rem;
 }
 @media (min-width: 1280px) {
     * {
@@ -23,36 +23,44 @@ style.textContent = `
         overflow: auto !important;
     }
 }
+
+#my-floating-button,
+#my-speech-button,
+#my-stop-button {
+    background: white;
+    padding: 10px;
+}
 `;
 document.head.append(style);
 
+const buttonStyle = `
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    z-index: 1000;  // Ensure the button is above other elements
+    padding: 10px 20px;
+    cursor: 'pointer';
+    border: none;
+    border-radius: '4px';
+    box-shadow: '0 2px 5px rgba(0, 0, 0, 0.3)'; // Material Design box shadow
+`;
+
+const setIcon = (buttonId, imgPath) => {
+    let img = document.createElement('img');
+    img.src = chrome.runtime.getURL(imgPath);
+    img.style.height = '20px';
+    img.style.width = '20px';
+    let button = document.getElementById(buttonId);
+    button.innerHTML = '';
+    button.appendChild(img);
+};
 
 // Create a new button element
 let button = document.createElement('button');
-button.textContent = 'C';
-button.title = 'Copy last generated message';
-
-// Set button styles
-button.style.position = 'fixed';
-button.style.top = '20px';
-button.style.right = '20px';
-button.style.zIndex = '1000';  // Ensure the button is above other elements
-button.style.padding = '10px 20px';
-button.style.fontSize = '15px';
-button.style.cursor = 'pointer';
-
-// Set additional button styles
-button.style.backgroundColor = '#9c27b0'; // Violet color
-button.style.border = 'none';
-button.style.borderRadius = '4px';
-button.style.color = 'white';
-button.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.3)'; // Material Design box shadow
-
-// Set an id for the button
 button.id = 'my-floating-button';
-
-// Append the button to the body
+button.style = buttonStyle;
 document.body.append(button);
+setIcon('my-floating-button', 'copy.svg');
 
 // Add event listener for the click event
 button.addEventListener('click', () => {
@@ -65,9 +73,9 @@ button.addEventListener('click', () => {
         let lastElement = elements[elements.length - 1];
 
         // Copy its text content to the clipboard
-        navigator.clipboard.writeText(lastElement.textContent).then(function() {
+        navigator.clipboard.writeText(lastElement.textContent).then(function () {
             console.log('Text copied to clipboard');
-        }).catch(function(err) {
+        }).catch(function (err) {
             console.error('Failed to copy text: ', err);
         });
     }
@@ -75,87 +83,13 @@ button.addEventListener('click', () => {
 
 // Create a new button for Text-to-Speech
 let speechButton = document.createElement('button');
-speechButton.textContent = 'S';
-speechButton.title = 'Read last generated message';
-
-// Set button styles
-speechButton.style.position = 'fixed';
-speechButton.style.top = '70px'; // Adjusted this line
-speechButton.style.right = '20px';
-speechButton.style.zIndex = '1000';  // Ensure the button is above other elements
-speechButton.style.padding = '10px 20px';
-speechButton.style.fontSize = '15px';
-speechButton.style.cursor = 'pointer';
-
-// Set additional button styles
-speechButton.style.backgroundColor = '#9c27b0'; // Violet color
-speechButton.style.border = 'none';
-speechButton.style.borderRadius = '4px';
-speechButton.style.color = 'white';
-speechButton.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.3)'; // Material Design box shadow
-
-// Set an id for the button
 speechButton.id = 'my-speech-button';
-
-// Append the button to the body
+speechButton.style = buttonStyle;
 document.body.append(speechButton);
-
-// Language selection dropdown
-let languageDropdown = document.createElement('select');
-languageDropdown.id = 'language-selection';
-languageDropdown.style.position = 'fixed';
-languageDropdown.style.top = '130px';
-languageDropdown.style.right = '20px';
-languageDropdown.style.zIndex = '1000';
-let languages = ['en-US', 'fr-FR', 'ar-SA'];
-for (let language of languages) {
-    let option = document.createElement('option');
-    option.value = language;
-    option.text = language;
-    languageDropdown.appendChild(option);
-}
-document.body.append(languageDropdown);
-languageDropdown.title = 'Select language for text-to-speech';
-
-// Create a new button for stopping speech
-let stopButton = document.createElement('button');
-stopButton.textContent = 'T';
-stopButton.title = 'Stop reading';
-
-// Set button styles
-stopButton.style.position = 'fixed';
-stopButton.style.top = '180px'; // Adjusted this line
-stopButton.style.right = '20px';
-stopButton.style.zIndex = '1000';
-stopButton.style.padding = '10px 20px';
-stopButton.style.fontSize = '15px';
-stopButton.style.cursor = 'pointer';
-
-// Set additional button styles
-stopButton.style.backgroundColor = '#9c27b0';
-stopButton.style.border = 'none';
-stopButton.style.borderRadius = '4px';
-stopButton.style.color = 'white';
-stopButton.style.boxShadow = '0 2px 5px rgba(0, 0, 60, 0.3)';
-
-// Set an id for the button
-stopButton.id = 'my-stop-button';
-
-// Hide the stop button initially
-stopButton.style.display = 'none';
-
-// Append the button to the body
-document.body.append(stopButton);
+setIcon('my-speech-button', 'play.svg');
 
 // Add event listener for the speech button click event
 speechButton.addEventListener('click', () => {
-    // Hide the speech button and language dropdown
-    speechButton.style.display = 'none';
-    languageDropdown.style.display = 'none';
-
-    // Show the stop button
-    stopButton.style.display = 'block';
-
     // Get all elements with the specified class name
     let elements = document.getElementsByClassName('flex flex-grow flex-col gap-3');
 
@@ -165,30 +99,29 @@ speechButton.addEventListener('click', () => {
         let lastElement = elements[elements.length - 1];
 
         // Use speech synthesis to speak the text content
+        let lang = dropdown.value;
         let msg = new SpeechSynthesisUtterance(lastElement.textContent);
-        let selectedOption = languageDropdown.options[languageDropdown.selectedIndex].value;
-        msg.lang = selectedOption;
-        msg.onend = function (event) {
-            // Speech has ended, show the speech button and language dropdown
-            speechButton.style.display = 'block';
-            languageDropdown.style.display = 'block';
-
-            // Hide the stop button
-            stopButton.style.display = 'none';
-        }
+        msg.lang = lang;
         window.speechSynthesis.speak(msg);
+        stopButton.style.display = 'block';
+        speechButton.style.display = 'none';
     }
 });
 
+// Create a new button for stopping speech
+let stopButton = document.createElement('button');
+stopButton.id = 'my-stop-button';
+stopButton.style = buttonStyle;
+document.body.append(stopButton);
+setIcon('my-stop-button', 'stop.svg');
+
 // Add event listener for the stop button click event
 stopButton.addEventListener('click', () => {
-    // Speech stopped, show the speech button and language dropdown
-    speechButton.style.display = 'block';
-    languageDropdown.style.display = 'block';
-
-    // Hide the stop button
-    stopButton.style.display = 'none';
-
     // Stop speech
     window.speechSynthesis.cancel();
+    stopButton.style.display = 'none';
+    speechButton.style.display = 'block';
 });
+
+// Initially hide the stop button
+stopButton.style.display = 'none';
