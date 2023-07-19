a25ed7f66f03795ce26c0123287628ef857e1acc
saidonnet <saidonnet@gmail.com>
2023-07-19 03:23:55 +0100

    Add files via upload

--- content.js
+++ content.js
@@ -3,22 +3,24 @@ style.textContent = `
 ::-webkit-scrollbar {
 	width: 1rem;
 }
-
 @media (min-width: 1280px) {
     * {
         max-width: unset !important;
+        overflow: auto !important;
     }
 }
 
 @media (min-width: 1024px) {
     * {
         max-width: unset !important;
+        overflow: auto !important;
     }
 }
 
 @media (min-width: 768px) {
     * {
         max-width: unset !important;
+        overflow: auto !important;
     }
 }
 `;
